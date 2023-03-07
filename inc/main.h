3d49ae77be18986118e184e16e55121f61a7ca8c
rocio469 <rociozatarain@hotmail.com>
2023-03-06 22:42:10 -0600

    se añadio el archivo principal y su header

---
+++ inc/main.h
@@ -0,0 +1,6 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#define WELCOME "HELLO\n"
+
+#endif /* __MAIN_H__ */
---
+++ source/expmachine.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * @Compilar: desde la carpeta ejemplo -> make all, 
+ *              se genera un binario llamado app.bin
+ * 
+ * @Ejecutar: desde la carpeta ejemplo -> ./app.bin
+ * 
+ * @Ejercicio: Ordenar los elementos del array dado y mostrar el resultado en consola
+ * 
+ * @TODO: - Hacer un algoritmo que imite el funcionamiento de una maquina expendedora
+ *        - Desplegar los productos disponibles en la maquina y precios (definir qué productos 
+ *          contiene y cuanto de cada uno).
+ *        - Pedir al usuario que ingrese el ID del producto asi como una cantidad de dinero
+ *        - La maquina debe ser capaz de desplegar el cambio a devolver, actualizar los productos
+ *          disponibles cuando se venden, eliminar de la lista desplegada los productos agotados.
+ * 
+ */
+
+int main(void) 
+{
+    return 0;
+}
